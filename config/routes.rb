d55e56c78372f2c5fc0f826f7a2609579a45b967
Matt Lashinsky <malashinsky@gmail.com>
2016-02-20 17:14:29 +0000

    message

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,11 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stocks', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
-  # Example of regular route:
+  get 'search_friends', to: 'users#search'
+  post 'add_friend', to: 'users#add_friend'
+  
+  
+  # Example of regular route:   
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
